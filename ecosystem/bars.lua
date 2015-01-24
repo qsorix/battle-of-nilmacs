545197a59e1fac18ee1ee1527453d6d0d64602c4
Tomasz Rydzyński <qsorix@rydznet.pl>
2015-01-24 20:18:55 +0100

    Prevent starvation of Bars
    Bars always ate too much grass so I've added restrictions in their code to
    prevent their starvation after consuming all food. The restrictions were a bit
    too severe, causing bars to starve surrounded by grass. This commit changes how
    these restrictions work. Now Bars will eat a lot, but never everything, so the
    grass can keep growing.

--- ecosystem/bars.lua
+++ ecosystem/bars.lua
@@ -4,15 +4,25 @@ species.display_letter = 'b'
 species.display_color = 'blue'
 species.initial_energy = 29
 
+local function count_grass(sees)
+    local count = 0
+    for _, c in ipairs(sees) do
+        if c.species == "grass" then
+            count = count + 1
+        end
+    end
+    return count
+end
+
 function species.brain()
     local dx, dy
 
     if Self.energy > 40 then
-        return Decision.Breed(20)
+        return Decision.Breed(29)
     end
 
     local sees = Action.Look(2)
-    if Self.energy < 5 or #sees > 11 then
+    if count_grass(sees) > 2 then
         local dx, dy, c = Algo.closest("grass", Self, sees)
         if (c) then
             if c.alive then
